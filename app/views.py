dd496cb9c0e7d8e1b91a0324032ce5e097d14364
mosius27 <mosius27@yandex.ru>
2023-03-24 07:20:21 +0300

    .

---
+++ app/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import os
+import datetime
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': '',
+        'Показать содержимое рабочей директории': ''
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    files = os.listdir()
+    return HttpResponse(f'Files in the current directory: {files}')
